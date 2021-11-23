ff3b4de3cec47f127d5e994ad52c3f743aabdd63
charlesbarkleyy <89173566+charlesbarkleyy@users.noreply.github.com>
2021-11-22 21:45:34 -0500

    Added name popup

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //for loop to create new color scheme
-
 const colorPicker = function(){
     let x = document.getElementsByClassName('newcolor');
     let hex = (Math.random() * 0xfffff * 1000000).toString(16);
@@ -8,6 +7,8 @@ const colorPicker = function(){
         x[i].style.backgroundColor = hex;
     }
 }
+ document.getElementById("personalize").onclick = colorPicker;
+
 
 // changing 3 bar hamburger to x
 const burger = document.querySelector('.hamburger');
@@ -17,8 +18,22 @@ function mobilemenu() {
     nav.classList.toggle("active");
     
 };
-
 burger.addEventListener("click",mobilemenu)
+
+// Name pop up window
+let names = [];
+let nameForm = document.getElementById('nameForm');
+function saveNames(){
+   let username = document.getElementById("inputName").value
+   names.push(username);
+   console.log(names)
+   document.querySelector(".namePopup").classList.toggle("active");
+}
+
+
+nameForm.addEventListener("submit",saveNames)
+
+
 
 
 
